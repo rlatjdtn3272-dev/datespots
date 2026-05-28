7873b07be9b9458a586fa477dbe512f0ac4de1f9
rlatjdtn3272-dev <rlatjdtn3272@gmail.com>
2026-05-28 22:30:11 +0900

    Update places-set.js

--- api/places-set.js
+++ api/places-set.js
@@ -9,10 +9,11 @@ export default async function handler(req, res) {
     const { data } = req.body;
     const url = process.env.KV_REST_API_URL;
     const token = process.env.KV_REST_API_TOKEN;
-    const r = await fetch(`${url}/set/places`, {
+
+    const r = await fetch(`${url}/pipeline`, {
       method: "POST",
       headers: { Authorization: `Bearer ${token}`, "Content-Type": "application/json" },
-      body: JSON.stringify(["SET", "places", data])
+      body: JSON.stringify([["SET", "places", data]])
     });
     const result = await r.json();
     res.status(200).json({ ok: true, result });
